d55fc3f7b7ea5d0a5b94bb4012afd12e361108fe
ricky <ricky@LAPTOP-CP0I48O5>
2020-11-04 14:36:34 -0800

    Test For Shasta GUI

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,21 @@
+/*
+ * This is a Test 123 ...... Practice 
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author ricky
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
