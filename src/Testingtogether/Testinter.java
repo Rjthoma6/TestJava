04d395b93729a4a31f538efb6ba0076732ce1182
daeyoungchoe <61814150+daeyoungchoe@users.noreply.github.com>
2020-11-18 04:17:27 -0600

    Home/Launch Andrew Choe

--- src/Testingtogether/Testinter.java
+++ src/Testingtogether/Testinter.java
@@ -104,7 +104,7 @@ public class Testinter extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new customer().setVisible(true);
+        new Home().setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
